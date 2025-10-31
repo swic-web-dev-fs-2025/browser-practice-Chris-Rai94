25859b36c0276b77cf524d580b9d2a9118ff37e2
Christopher Stokley <christopher.stokley@student.swic.edu>
2025-10-31 14:34:17 -0500

    Revert to original commit for hw

--- src/main.js
+++ src/main.js
@@ -1,37 +1,7 @@
 import "./style.css";
-// Variables - same as console JavaScript
-let clickCount = 0;
 
-// Get references to HTML elements
-const greetingElement = document.getElementById("greeting");
-const button1 = document.getElementById("change-greeting");
-const button2 = document.getElementById("reset-greeting");
-const countElement = document.getElementById("click-count");
-const inputElement = document.getElementById("manual-greeting");
-// Function - same logic as console JavaScript
-function updateGreeting() {
-  clickCount++;
-  greetingElement.textContent = "Hello, Browser JavaScript!";
-  countElement.textContent = `Button clicks: ${clickCount} `;
-  if (clickCount === 5) {
-    greetingElement.style.color = "red";
-  }
-}
-
-const resetGreeting = () => {
-  clickCount = 0;
-  greetingElement.textContent = "Hello, Console JavaScript!";
-  countElement.textContent = `Button clicks: ${clickCount} `;
-};
-
-const manualGreeting = () => {
-  greetingElement.textContent = `${inputElement.value}`;
-  if (inputElement.value === "") {
-    greetingElement.textContent = "Hello, Console JavaScript!";
-  }
-};
-
-// Event listener - this is new! Responds to user interaction
-button1.addEventListener("click", updateGreeting);
-button2.addEventListener("click", resetGreeting);
-inputElement.addEventListener("change", manualGreeting);
+document.querySelector("#app").innerHTML = `
+  <h1 class="text-3xl font-bold underline">
+    Hello Vite!
+  </h1>
+`;
